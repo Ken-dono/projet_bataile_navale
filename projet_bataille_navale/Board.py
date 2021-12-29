e44293cb48489a6fab7db70133c298d76b20e3ff
Diabltica <Diabltica@gmail.com>
2021-12-29 09:33:52 +0100

    remove useless loop

--- projet_bataille_navale/Board.py
+++ projet_bataille_navale/Board.py
@@ -5,7 +5,7 @@ def is_place_valid(x, y, layer):
         return False
 
 
-def facing_coordinate(x, y, facing, size):
+def facing_coordinates(x, y, facing, size):
     x2 = x
     y2 = y
     x3 = x
@@ -37,28 +37,28 @@ class Board:
         self.board = [self.layer_1, self.layer_2, self.layer_3]
 
     def place_sub(self, x, y, sub, facing, layer):
-        for i in range(3):
-            if layer == i + 1:
-                if sub == 1:
-                    if is_place_valid(x, y, self.board[i]):
-                        self.board[i][x][y] = 1
-                    else:
-                        print("Erreur case invalide")
-                elif sub == 2:
-                    x2, y2 = facing_coordinate(x, y, facing, 2)
-                    if is_place_valid(x, y, self.board[i]) and \
-                            is_place_valid(x2, y2, self.board[i]):
-                        self.board[i][x][y] = 1
-                        self.board[i][x2][y2] = 1
-                    else:
-                        print("Erreur case invalide")
-                elif sub == 3:
-                    x2, y2, x3, y3 = facing_coordinate(x, y, facing, 3)
-                    if is_place_valid(x, y, self.board[i]) and \
-                            is_place_valid(x2, y2, self.board[i]) and \
-                            is_place_valid(x3, y3, self.board[i]):
-                        self.board[i][x][y] = 1
-                        self.board[i][x2][y2] = 1
-                        self.board[i][x3][y3] = 1
-                    else:
-                        print("Erreur case invalide")
+        if 0 > layer or layer > 2:
+            return -1
+        if sub == 1:
+            if is_place_valid(x, y, self.board[layer]):
+                self.board[layer][x][y] = 1
+            else:
+                print("Erreur case invalide")
+        elif sub == 2:
+            x2, y2 = facing_coordinates(x, y, facing, sub)
+            if is_place_valid(x, y, self.board[layer]) and \
+                    is_place_valid(x2, y2, self.board[layer]):
+                self.board[layer][x][y] = 1
+                self.board[layer][x2][y2] = 1
+            else:
+                print("Erreur case invalide")
+        elif sub == 3:
+            x2, y2, x3, y3 = facing_coordinates(x, y, facing, sub)
+            if is_place_valid(x, y, self.board[layer]) and \
+                    is_place_valid(x2, y2, self.board[layer]) and \
+                    is_place_valid(x3, y3, self.board[layer]):
+                self.board[layer][x][y] = 1
+                self.board[layer][x2][y2] = 1
+                self.board[layer][x3][y3] = 1
+            else:
+                print("Erreur case invalide")
