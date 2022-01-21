7f27f861a9e77a44ab7085e42f2ad76dc5d3ef0d
Diabltica <Diabltica@gmail.com>
2022-01-21 08:39:14 +0100

    patch bug is_place_valid and test facing and place sub

--- src/Board.py
+++ src/Board.py
@@ -1,5 +1,5 @@
 def is_place_valid(x, y, layer):
-    if layer[x][y] == 0 and 0 > x > 9 and 0 > y > 5:
+    if layer[x][y] == '0' and 9 > x >= 0 and 5 > y >= 0:
         return True
     else:
         return False
@@ -10,16 +10,17 @@ def facing_coordinates(x, y, facing, size):
     y2 = y
     x3 = x
     y3 = y
-    if facing == 's':
+    if facing == 'e':
         y2 += 1
         y3 += 2
-    elif facing == 'n':
+    elif facing == 'w':
         y2 -= 1
         y3 -= 2
-    elif facing == 'w':
+    elif facing == 'n':
         x2 -= 1
         x3 -= 2
-    elif facing == 'e':
+
+    elif facing == 's':
         x2 += 1
         x3 += 2
     if size == 2:
@@ -36,20 +37,21 @@ class Board:
         self.layer_3 = [['0' for i in range(10)] for j in range(5)]
         self.board = [self.layer_1, self.layer_2, self.layer_3]
 
-    def place_sub(self, x, y, sub, facing, layer):      
+    def place_sub(self, x, y, sub, facing, layer):
         if 0 > layer or layer > 2:
             return -1
         if sub == 1:
             if is_place_valid(x, y, self.board[layer]):
-                self.board[layer][x][y] = 1
+                self.board[layer][x][y] = '1'
             else:
                 print("Erreur case invalide")
         elif sub == 2:
             x2, y2 = facing_coordinates(x, y, facing, sub)
+            print(x, x2, y, y2)
             if is_place_valid(x, y, self.board[layer]) and \
                     is_place_valid(x2, y2, self.board[layer]):
-                self.board[layer][x][y] = 1
-                self.board[layer][x2][y2] = 1
+                self.board[layer][x][y] = '1'
+                self.board[layer][x2][y2] = '1'
             else:
                 print("Erreur case invalide")
         elif sub == 3:
@@ -57,21 +59,21 @@ class Board:
             if is_place_valid(x, y, self.board[layer]) and \
                     is_place_valid(x2, y2, self.board[layer]) and \
                     is_place_valid(x3, y3, self.board[layer]):
-                self.board[layer][x][y] = 1
-                self.board[layer][x2][y2] = 1
-                self.board[layer][x3][y3] = 1
+                self.board[layer][x][y] = '1'
+                self.board[layer][x2][y2] = '1'
+                self.board[layer][x3][y3] = '1'
             else:
                 print("Erreur case invalide")
 
     def draw_board(self):
         legend = [str(i) for i in range(10)]
         for i in range(3):
-            print("Couche :",i + 1)
-            print("   ",end='')
+            print("Couche :", i + 1)
+            print("   ", end='')
             for y in range(10):
-                print("  "+legend[y]+"  ",end='')
+                print("  " + legend[y] + "  ", end='')
             print('')
-            
+
             for v in range(5):
-                print(v,end="  ")
+                print(v, end="  ")
                 print(self.board[i][v])
--- src/Main.py
+++ src/Main.py
@@ -2,5 +2,6 @@ from Player import Player
 from Board import Board
 
 board = Board()
+board.place_sub(2,2,3,'s',1)
 board.draw_board()
 
