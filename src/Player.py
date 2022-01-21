8d5f3b6f3220dd0efbf52f7fc00ce867e4705ad8
Diabltica <Diabltica@gmail.com>
2022-01-21 20:02:04 +0100

    reenforce User input

--- src/Player.py
+++ src/Player.py
@@ -1,7 +1,11 @@
 from Board import Board
 from Submarine import SubMarine
 
-
+def is_input_digit(input):
+    if input.isdigit():
+        return int(input)
+    else:
+        return -1
 class Player:
     def __init__(self):
         Board.__init__(self)
@@ -9,9 +13,9 @@ class Player:
     def ask_coordinate(self, board):
         valid_layer = False
         while not valid_layer:
-            x = int(input("X : "))
-            y = int(input("Y : "))
-            layer = int(input("Layer : "))
+            x = is_input_digit(input("X : "))
+            y = is_input_digit(input("Y : "))
+            layer = is_input_digit(input("Layer : "))
             if 0 < layer < 4 and 0 <= y <= 9 and 0 <= x < 5:
                 valid_layer = True
             else:
