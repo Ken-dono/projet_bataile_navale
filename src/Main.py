a53343b9bd601b4f1ddb2cc16b1f2b9808f08375
Ken-dono <kendono0206@gmail.com>
2022-01-21 11:28:20 +0100

    Suppression classe Submarine

--- src/Main.py
+++ src/Main.py
@@ -11,18 +11,19 @@ current_board = Board()
 is_game_finished = False
 while is_game_finished == False:
     for player in [1, 2]:
+        print("Player ", player, "'s turn")
         if player == 1:
-            print("Player ", player, "'s turn")
             current_player = player_1
             current_board = board_2
             current_player.fire_torpedo(current_board)
         elif player == 2:
-            print("Player ", player, "'s turn")
             current_player = player_2
             current_board = board_1
             current_player.fire_torpedo(current_board)
+            # FIN DE BOUCLE POUR TEST
+            is_game_finished == True
         if is_game_finished == True:
             break
     player = 1
 
-    #current_board.draw_board()
+    # current_board.draw_board()
