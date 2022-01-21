1f8dda1738bc2057aa12b09ef8903a80751f73a7
Ken-dono <kendono0206@gmail.com>
2022-01-21 10:19:47 +0100

    Premier commit : ask_coordinate et debut travail main

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,11 @@
-from Player import Player
 from Board import Board
+from Player import Player
 
-board = Board()
-board.draw_board()
+board_1 = Board()
+player_1 = Player()
 
+is_game_finished = False
+while is_game_finished == False:
+    player_1.fire_torpedo(board_1)
+    is_game_finished = True
+# board.draw_board()
--- src/Player.py
+++ src/Player.py
@@ -1,7 +1,31 @@
 from Board import Board
 from Submarine import SubMarine
 
-class Player:
-	def __init__(self):
-		Board.__init__(self)
 
+class Player:
+    def __init__(self):
+        Board.__init__(self)
+        SubMarine.__init__(self)
+
+    def ask_coordinate(self, board):
+        valid_layer = False
+        while valid_layer == False:
+            x = int(input("X : "))
+            y = int(input("Y : "))
+            layer = int(input("Layer : "))
+            if layer == 1 and 0 <= x <= 9 and 0 <= y < 5:
+                layer = board.board[layer - 1]
+                valid_layer = True
+            elif layer == 2 and 0 <= x <= 9 and 0 <= y < 5:
+                valid_layer = True
+                layer = board.board[layer - 1]
+            elif layer == 3 and 0 <= x <= 9 and 0 <= y < 5:
+                valid_layer = True
+                layer = board.board[layer - 1]
+            else:
+                print("Invalid coordinates")
+        return x, y, layer
+
+    def fire_torpedo(self, board):
+        x, y, layer = Player.ask_coordinate(Player, board)
+        print("FIRE !!!!!!!")
