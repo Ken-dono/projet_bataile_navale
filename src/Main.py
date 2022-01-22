25765242c9bb4c288e9510a911b7b8fe9906aa25
Diabltica <Diabltica@gmail.com>
2022-01-22 10:55:39 +0100

    change function parameter

--- src/Main.py
+++ src/Main.py
@@ -8,17 +8,17 @@ Tplayer = [Player(),Player()]
 Tboard = [Board(),Board()]
 TSub = [SubMarine(),SubMarine()]
 count = 0
+Tboard[1].place_sub(1,1,1,'w',1)
 while not is_game_finished:
     p_index = count % 2
     count +=1
     e_index = count % 2
     print("Player ", p_index + 1, "'s turn")
-    Tboard[p_index].draw_board() #Draw the board before choosing coordinate
+    Tboard[e_index].draw_board() #Draw the board before choosing coordinate
     x,y,layer = Tplayer[p_index].fire_torpedo(Tboard[e_index]) #Shoot
-    Tboard[p_index].clear_info() #Clear previous shot information
-    TSub[p_index].shot_result(x,y,layer,Tboard[p_index]) #Calculate the result
-    Tboard[p_index].draw_board() #Draw the update board
-
+    Tboard[e_index].clear_info() #Clear previous shot information
+    TSub[e_index].shot_result(x,y,layer,Tboard[e_index]) #Calculate the result
+    Tboard[e_index].draw_board() #Draw the update board
 
     # FIN DE BOUCLE POUR TEST
     is_game_finished = True
